a1bc1ec014d698065ea4dd0cd9161028d9636108
djkoser <koser.david@gmail.com>
2021-04-12 22:23:38 -0500

    Fully test and troubleshoot backend, backend complete

--- db/weed/weedsByTypeKw.sql
+++ db/weed/weedsByTypeKw.sql
@@ -4,6 +4,6 @@ FROM
   weed
 WHERE
   veg_type = $1
-  AND (common_name LIKE % $1 %
-    OR botanical_name LIKE % $1 %);
+  AND (common_name LIKE '%'||$2||'%')
+    OR (botanical_name LIKE '%'||$2||'%');
 
