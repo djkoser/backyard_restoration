5116a4f0df77eb9d5e103c78a605a58861773064
djkoser <koser.david@gmail.com>
2021-04-12 22:31:28 -0500

    Create frontend file structure and begin importing relevant components

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
 import { Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import MyAccount from "./components/MyAccount";
+import Register from "./components/Register";
+import WeedPage from "./components/WeedPage";
 
 export default (
   <Switch>
