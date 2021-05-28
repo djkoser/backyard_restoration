da34e30e6c411a205a44e64b78f5d2743637ac17
David Koser <koser.david@gmail.com>
2021-05-27 20:26:53 -0500

    Remove linebreak style from .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
         "quotes": [
             "error",
             "double"
