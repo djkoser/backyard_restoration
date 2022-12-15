8811d6ec2c04217ed92e6628bda552e43217911e
David Koser <koser.david@gmail.com>
2022-12-15 13:47:05 -0600

    chore: pull in amplify backend

--- amplify/backend/function/AdminQueriesae8f1082/src/app.js
+++ amplify/backend/function/AdminQueriesae8f1082/src/app.js
@@ -1,257 +1,130 @@
-'use strict';
-var __assign =
-  (this && this.__assign) ||
-  function () {
-    __assign =
-      Object.assign ||
-      function (t) {
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
         return t;
-      };
+    };
     return __assign.apply(this, arguments);
-  };
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __generator =
-  (this && this.__generator) ||
-  function (thisArg, body) {
-    var _ = {
-        label: 0,
-        sent: function () {
-          if (t[0] & 1) throw t[1];
-          return t[1];
-        },
-        trys: [],
-        ops: []
-      },
-      f,
-      y,
-      t,
-      g;
-    return (
-      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
-      typeof Symbol === 'function' &&
-        (g[Symbol.iterator] = function () {
-          return this;
-        }),
-      g
-    );
-    function verb(n) {
-      return function (v) {
-        return step([n, v]);
-      };
-    }
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
-      if (f) throw new TypeError('Generator is already executing.');
-      while ((g && ((g = 0), op[0] && (_ = 0)), _))
-        try {
-          if (
-            ((f = 1),
-            y &&
-              (t =
-                op[0] & 2
-                  ? y['return']
-                  : op[0]
-                  ? y['throw'] || ((t = y['return']) && t.call(y), 0)
-                  : y.next) &&
-              !(t = t.call(y, op[1])).done)
-          )
-            return t;
-          if (((y = 0), t)) op = [op[0] & 2, t.value];
-          switch (op[0]) {
-            case 0:
-            case 1:
-              t = op;
-              break;
-            case 4:
-              _.label++;
-              return { value: op[1], done: false };
-            case 5:
-              _.label++;
-              y = op[1];
-              op = [0];
-              continue;
-            case 7:
-              op = _.ops.pop();
-              _.trys.pop();
-              continue;
-            default:
-              if (
-                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
-                (op[0] === 6 || op[0] === 2)
-              ) {
-                _ = 0;
-                continue;
-              }
-              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
-                _.label = op[1];
-                break;
-              }
-              if (op[0] === 6 && _.label < t[1]) {
-                _.label = t[1];
-                t = op;
-                break;
-              }
-              if (t && _.label < t[2]) {
-                _.label = t[2];
-                _.ops.push(op);
-                break;
-              }
-              if (t[2]) _.ops.pop();
-              _.trys.pop();
-              continue;
-          }
-          op = body.call(thisArg, _);
-        } catch (e) {
-          op = [6, e];
-          y = 0;
-        } finally {
-          f = t = 0;
-        }
-      if (op[0] & 5) throw op[1];
-      return { value: op[0] ? op[1] : void 0, done: true };
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
-var express_1 = __importDefault(require('express'));
-var body_parser_1 = __importDefault(require('body-parser'));
-var middleware_1 = __importDefault(
-  require('aws-serverless-express/middleware')
-);
-var cognitoActions_1 = require('./cognitoActions');
-var app = (0, express_1['default'])();
-app.use(body_parser_1['default'].json());
-app.use(body_parser_1['default'].urlencoded({ extended: true }));
-app.use(middleware_1['default'].eventContext());
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var middleware_1 = __importDefault(require("aws-serverless-express/middleware"));
+var cognitoActions_1 = require("./cognitoActions");
+var app = (0, express_1["default"])();
+app.use(body_parser_1["default"].json());
+app.use(body_parser_1["default"].urlencoded({ extended: true }));
+app.use(middleware_1["default"].eventContext());
 // Enable CORS for all methods
 app.use(function (request, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', '*');
-  //intercept the OPTIONS call so we don't double up on calls to the integration
-  if ('OPTIONS' === request.method) {
-    res.send(200);
-  } else {
-    next();
-  }
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', '*');
+    //intercept the OPTIONS call so we don't double up on calls to the integration
+    if ('OPTIONS' === request.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
 // Only perform tasks if the user is in a specific group
 var allowedGroup = process.env.GROUP;
 var checkGroup = function (req, _res, next) {
-  if (typeof allowedGroup === 'undefined' || allowedGroup === 'NONE') {
-    return next();
-  }
-  // Fail if group enforcement is being used
-  if (req.apiGateway.event.requestContext.authorizer.claims['cognito:groups']) {
-    var groups =
-      req.apiGateway.event.requestContext.authorizer.claims[
-        'cognito:groups'
-      ].split(',');
-    if (!(allowedGroup && groups.indexOf(allowedGroup) > -1)) {
-      var err = new Error(
-        'User does not have permissions to perform administrative tasks'
-      );
-      next(err);
+    if (typeof allowedGroup === 'undefined' || allowedGroup === 'NONE') {
+        return next();
     }
-  } else {
-    var err = __assign(
-      __assign(
-        {},
-        new Error(
-          'User does not have permissions to perform administrative tasks'
-        )
-      ),
-      { statusCode: 403 }
-    );
-    next(err);
-  }
-  next();
+    // Fail if group enforcement is being used
+    if (req.apiGateway.event.requestContext.authorizer.claims['cognito:groups']) {
+        var groups = req.apiGateway.event.requestContext.authorizer.claims['cognito:groups'].split(',');
+        if (!(allowedGroup && groups.indexOf(allowedGroup) > -1)) {
+            var err = new Error("User does not have permissions to perform administrative tasks");
+            next(err);
+        }
+    }
+    else {
+        var err = __assign(__assign({}, new Error("User does not have permissions to perform administrative tasks")), { statusCode: 403 });
+        next(err);
+    }
+    next();
 };
 app.all('*', checkGroup);
-app['delete']('/deleteUserIfUnconfirmed', function (req, res, next) {
-  return __awaiter(void 0, void 0, void 0, function () {
+app["delete"]('/deleteUserIfUnconfirmed', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
     var err, response, err_1;
     return __generator(this, function (_a) {
-      switch (_a.label) {
-        case 0:
-          if (!req.body.username) {
-            err = __assign(
-              __assign(
-                {},
-                new Error('username is required within request body')
-              ),
-              { statusCode: 400 }
-            );
-            return [2 /*return*/, next(err)];
-          }
-          _a.label = 1;
-        case 1:
-          _a.trys.push([1, 3, , 4]);
-          return [
-            4 /*yield*/,
-            (0, cognitoActions_1.deleteUserIfUnconfirmed)(req.body.username)
-          ];
-        case 2:
-          response = _a.sent();
-          res.status(200).json(response);
-          return [3 /*break*/, 4];
-        case 3:
-          err_1 = _a.sent();
-          next(err_1);
-          return [3 /*break*/, 4];
-        case 4:
-          return [2 /*return*/];
-      }
+        switch (_a.label) {
+            case 0:
+                if (!req.body.username) {
+                    err = __assign(__assign({}, new Error('username is required within request body')), { statusCode: 400 });
+                    return [2 /*return*/, next(err)];
+                }
+                _a.label = 1;
+            case 1:
+                _a.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, (0, cognitoActions_1.deleteUserIfUnconfirmed)(req.body.username)];
+            case 2:
+                response = _a.sent();
+                res.status(200).json(response);
+                return [3 /*break*/, 4];
+            case 3:
+                err_1 = _a.sent();
+                next(err_1);
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
+        }
     });
-  });
-});
+}); });
 // Error middleware must be defined last
 app.use(function (err, _req, res, _next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-  res.status(err.statusCode).json({ message: err.message }).end();
+    console.error(err.message);
+    if (!err.statusCode)
+        err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+    res.status(err.statusCode).json({ message: err.message }).end();
 });
 app.listen(3000, function () {
-  console.log('App started');
+    console.log('App started');
 });
-exports['default'] = app;
+exports["default"] = app;
