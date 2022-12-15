bb433357d23af8966e5f4dc01096d5110cb8471a
David Koser <koser.david@gmail.com>
2022-12-15 14:43:36 -0600

    fix: add command to globally install rimraf and copyfiles for execution of function pre-build scripts

--- amplify/backend/function/stripeDonationFunction/src/app.js
+++ amplify/backend/function/stripeDonationFunction/src/app.js
@@ -37,7 +37,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+        while (_) try {
             if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
             if (y = 0, t) op = [op[0] & 2, t.value];
             switch (op[0]) {
@@ -62,7 +62,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-exports.app = void 0;
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var middleware_1 = __importDefault(require("aws-serverless-express/middleware"));
@@ -71,16 +70,23 @@ var AWS = __importStar(require("aws-sdk"));
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
 // declare a new express app
-exports.app = (0, express_1["default"])();
-exports.app.use(body_parser_1["default"].json());
-exports.app.use(middleware_1["default"].eventContext());
+var app = (0, express_1["default"])();
+app.use(body_parser_1["default"].json());
+app.use(middleware_1["default"].eventContext());
 // Enable CORS for all methods
-exports.app.use(function (req, res, next) {
+app.use(function (request, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, OPTIONS');
     res.header('Access-Control-Allow-Headers', '*');
-    next();
+    //intercept the OPTIONS call so we don't double up on calls to the integration
+    if ('OPTIONS' === request.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
-exports.app.put('/donate', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
+app.put('/donate', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
     var Parameters, stripe, session, err_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -126,6 +132,14 @@ exports.app.put('/donate', function (req, res, next) { return __awaiter(void 0, 
         }
     });
 }); });
-exports.app.listen(3000, function () {
+// Error middleware must be defined last
+app.use(function (err, _req, res, _next) {
+    console.error(err.message);
+    if (!err.statusCode)
+        err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+    res.status(err.statusCode).json({ message: err.message }).end();
+});
+app.listen(3000, function () {
     console.log('App started');
 });
+exports["default"] = app;
